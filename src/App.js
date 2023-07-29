c6a431885a6015faa5795905a744f6ea9588472a
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-07-29 06:40:51 +0530

    banner setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Banner/>
     </div>
   );
 }
