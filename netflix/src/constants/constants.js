c5f92c9d252c698951ce450542e912a46bd08417
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-08-06 07:53:37 +0530

    latest movies updates added

---
+++ netflix/src/constants/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY ='3d59174c77b7403b0a07ad10545fb16b' 
+//image baseurl
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
