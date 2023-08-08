cbd462ac02976dce7bbfaeb29d34bed286272199
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-08-08 06:10:06 +0530

    dynamic movies list created

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar'
 import RowPost from './Components/RowPost/RowPost';
+import {action,Originals} from './urls'
 
 function App() {
   return (
     <div className="App" style={{background:'#000'}}>
       <NavBar/>
       <Banner/>
-      <RowPost title='Netflix Originals'/>
-      <RowPost title='Action' isSmall />
+      <RowPost url={Originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall />
     </div>
   );
 }
