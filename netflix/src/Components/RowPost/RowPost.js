fd7b4d34e8b74e1ee7d683b6e9c0013deba4d82f
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-08-08 06:46:36 +0530

    youtube trailor display

--- netflix/src/Components/RowPost/RowPost.js
+++ netflix/src/Components/RowPost/RowPost.js
@@ -1,27 +1,48 @@
 import React, { useEffect, useState } from 'react'
-import {imageUrl} from '../../constants/constants'
+import {API_KEY, imageUrl} from '../../constants/constants'
 import './RowPost.css'
 import axios from '../../axios'
+import Youtube from 'react-youtube'
 
 function RowPost(props) {
   const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState('')
   useEffect(()=>{
     axios.get(props.url).then((response)=>{
       // console.log(response.data)
       setMovies(response.data.results)
     })
   },[])
+
+  const handleMovie = (id)=>{
+    console.log('>>>>>>>>>>>>>>>>>>',id)
+    //getting youtube key
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}`).then((response)=>{
+      console.log(response.data,'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+      setUrlId(response.data.results[0])
+    })
+  }
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
   return (
     <div className='row'>
             <h2>{props.title}</h2>
             <div className='posters'>  
               {
                 movies.map(obj=>
-                  <img className={props.isSmall ? 'smallPoster' :'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
+                  <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' :'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
                 )
               }
                 
             </div>
+            { urlId && <Youtube videoId={urlId.key} opts={opts}/> }
         </div>
   )
 }
